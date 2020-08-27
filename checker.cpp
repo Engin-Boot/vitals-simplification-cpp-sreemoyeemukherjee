7d02d3842f13d8d15bde4bd12a041c78c5d485b8
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-08-27 11:06:14 +0530

    CCN limit 3

--- checker.cpp
+++ checker.cpp
@@ -1,14 +1,19 @@
 #include <assert.h>
-
+bool BPM(float bpm)
+{
+    return (bpm < 70 || bpm > 150);
+}
+bool SPO2(float spo2)
+{
+    return (spo2 < 90);
+}
+bool RespRate(float respRate)
+{
+    return (respRate < 30 || respRate > 95);
+}
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+    bool result = BPM(bpm) || SPO2(spo2) || RespRate(respRate);
+    return !result;
 }
 
 int main() {
