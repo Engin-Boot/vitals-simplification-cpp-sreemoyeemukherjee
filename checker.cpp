a25065b56027cf942ae44d12d334b97cbb0d8889
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2020-08-31 20:25:58 +0530

    Friday Class - Inheritance

--- checker.cpp
+++ checker.cpp
@@ -1,98 +1,103 @@
 #include <assert.h>
 #include <iostream>
-
+using namespace std;
 class AlertInterface
 {
-  public:
+public:
     virtual void raiseAlert(const char* vitalName, const char* level) = 0;
 };
-
 class AlertWithSMS: public AlertInterface
 {
-  public:
-    void raiseAlert(const char* vitalName, const char* level) override
+public:
+    void raiseAlert(const char* vitalName,const char* level) override
     {
-      std::cout << "SMS: " << vitalName << " " << level << std::endl;
+        cout << "SMS: " << vitalName << " " << level<<endl;     //WHAT
     }
 };
-
 class AlertWithSound: public AlertInterface
 {
-  public:
-    void raiseAlert(const char* vitalName, const char* level) override
+public:
+    void raiseAlert(const char* vitalName,const char* level) override
     {
-      std::cout << "Sound: " << vitalName << " " << level << std::endl;  //'what'
+        cout << "Sound: " << vitalName << " " << level<<endl;
     }
 };
-
 class AlertIntegrator : public AlertInterface
 {
-  private:
+private:
     AlertWithSMS smsAlerter;
     AlertWithSound soundAlerter;
-  public:
+public:
     void raiseAlert(const char* vitalName, const char* level) override
     {
-      smsAlerter.raiseAlert(vitalName, level);
-      soundAlerter.raiseAlert(vitalName, level);
+        smsAlerter.raiseAlert(vitalName,level);
+        soundAlerter.raiseAlert(vitalName,level);
     }
 };
-
 class RangeChecker
 {
-  private:
+private:
     int lower;
     int upper;
     const char* vitalName;
     AlertInterface* alerter;
-  public:
+public:
     RangeChecker(const char* name, int low, int up, AlertInterface* alerterPtr)
     {
-      vitalName = name;
-      lower = low;
-      upper = up;
-      alerter = alerterPtr;
+        vitalName = name;
+        lower = low;
+        upper = up;
+        alerter = alerterPtr;
     }
     void checkAgainstRange(float value)
     {
-      if(value < lower) {
-        alerter->raiseAlert(vitalName, "too low");  //'when'
-      } else if(value > upper) {
-        alerter->raiseAlert(vitalName, "too high");
-      }
+        if (value < lower) {
+            alerter->raiseAlert(vitalName, "too low");      //WHEN
+        }
+        else if (value > upper) {
+            alerter->raiseAlert(vitalName, "too high");
+        }
+        else
+        {
+           // alerter->raiseAlert(vitalName, "is ok"); // TOO MANY NOTIFICATIONS
+        }
     }
 };
-
 class VitalsIntegrator
 {
-  private:
+private:
     RangeChecker bpmChecker, spo2Checker, respChecker;
-  public:
-    VitalsIntegrator(AlertInterface* alertPtr): 
-      bpmChecker("pulse rate", 70, 150, alertPtr),
-      spo2Checker("spo2", 90, 101, alertPtr),
-      respChecker("resp rate", 30, 95, alertPtr)
-    {}
+public:
+    VitalsIntegrator(AlertInterface* alerterPtr) :bpmChecker("pulse rate", 70, 150, alerterPtr),
+        spo2Checker("spo2", 90, 101, alerterPtr), respChecker("resp rate", 30, 95, alerterPtr){}
     void checkAllVitals(float bpm, float spo2, float respRate)
     {
-      bpmChecker.checkAgainstRange(bpm);
-      spo2Checker.checkAgainstRange(spo2);
-      respChecker.checkAgainstRange(respRate);
+        bpmChecker.checkAgainstRange(bpm);
+        spo2Checker.checkAgainstRange(spo2);
+        respChecker.checkAgainstRange(respRate);
     }
 };
 
 int main() {
-  //AlertIntegrator alerter;  
-  AlertInterface* alerter1;             // base class pointer
-  alerter1 = new AlertWithSMS;          // Alert with SMS
-  VitalsIntegrator vitals1(alerter1);
-  
-  vitals1.checkAllVitals(80, 95, 60);       // All OK condition
-  vitals1.checkAllVitals(60, 90, 140);      // pulse rate 'too low', resp rate 'too high' 
+  /*  int alerConfig = 0;
+    // read from config file 0=>SMS, 1=>SOUND
+    AlertInterface* alerter = 0;
+    if (alerConfig == 0)
+    {
+        alerter = new AlertWithSMS;
+    }
+    else if (alerConfig == 1)
+    {
+        alerter = new AlertWithSound;
+    }
+    VitalsIntegrator vitals(alerter);*/
+    //AlertWithSMS alerter;
+    //AlertWithSound alerter;
+    //VitalsIntegrator vitals(&alerter);
 
-  AlertInterface* alerter2;             
-  alerter2 = new AlertWithSound;        // Alert with Sound
-  VitalsIntegrator vitals2(alerter2);
-  vitals2.checkAllVitals(160, 95, 60);      // pulse rate 'too high'
+    AlertIntegrator alerter;
+    VitalsIntegrator vitals(&alerter);
 
+    vitals.checkAllVitals(80, 95, 60); 
+    vitals.checkAllVitals(60, 90, 40);
 }
